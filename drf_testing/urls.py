2f2252fd91cc940cdc84d3c392579d883dcb4a8b
rrodgers <rrodgers0827@outlook.com>
2018-08-29 19:46:50 -0500

    Rename project

--- drf_testing/urls.py
+++ drf_testing/urls.py
@@ -1,4 +1,4 @@
-"""alb_drf_testing URL Configuration
+"""drf_testing URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
--- drf_testing/wsgi.py
+++ drf_testing/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for alb_drf_testing project.
+WSGI config for drf_testing project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alb_drf_testing.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_testing.settings')
 
 application = get_wsgi_application()
